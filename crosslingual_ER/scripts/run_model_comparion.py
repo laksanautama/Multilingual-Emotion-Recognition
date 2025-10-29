612d9d8e233912a0f1ce5019cc1fc8a2664000a4
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-10-29 16:37:28 +0100

    delete dev_set

--- crosslingual_ER/scripts/run_model_comparion.py
+++ crosslingual_ER/scripts/run_model_comparion.py
@@ -6,8 +6,7 @@ def run(keys: dict):
     print("Running crosslingual tasks with provided API keys...")
     try:
         test_dataset = load_target_test_data(DATA_CONFIG["TARGET_TEST_FILENAME"])
-        source_dataset_train = load_huggingface_dataset(DATA_CONFIG["SOURCE_HF_DATASET"], DATA_CONFIG["DATASET_LANGUAGES"], 'train', keys)
-        source_dataset_dev = load_huggingface_dataset(DATA_CONFIG["SOURCE_HF_DATASET"], DATA_CONFIG["DATASET_LANGUAGES"], 'test', keys)
+        source_dataset_train, pw_label_train = load_huggingface_dataset(DATA_CONFIG["SOURCE_HF_DATASET"], DATA_CONFIG["DATASET_LANGUAGES"], 'train', keys)
         print("Datasets loaded successfully.")
 
     except FileNotFoundError as e:
@@ -16,3 +15,4 @@ def run(keys: dict):
     
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+    
