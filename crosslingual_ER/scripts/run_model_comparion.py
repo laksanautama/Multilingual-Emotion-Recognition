ee13b07c34073a7286a23d7b5eb98db686dc4702
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-10-30 16:24:34 +0100

    Update run_model_comparion.py

--- crosslingual_ER/scripts/run_model_comparion.py
+++ crosslingual_ER/scripts/run_model_comparion.py
@@ -1,11 +1,16 @@
 from .model_configs import DATA_CONFIG, MODEL_PATHS, TRAINING_CONFIG
 from .data_loader import load_target_test_data, load_huggingface_dataset
+import torch
 
 def run(keys: dict):
+    # device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
+
     """Placeholder function for running crosslingual ER tasks."""
     print("Running crosslingual tasks with provided API keys...")
     try:
-        test_dataset = load_target_test_data(DATA_CONFIG["TARGET_TEST_FILENAME"])
+        test_dataset = load_target_test_data(DATA_CONFIG["TARGET_TEST_FILENAME"], cross_lingual=True)
         source_dataset_train, pw_label_train = load_huggingface_dataset(DATA_CONFIG["SOURCE_HF_DATASET"], DATA_CONFIG["DATASET_LANGUAGES"], 'train', keys)
         print("Datasets loaded successfully.")
 
