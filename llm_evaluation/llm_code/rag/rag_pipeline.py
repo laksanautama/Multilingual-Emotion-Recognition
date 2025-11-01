9918be68414e7b8fb806c9931eb741b108b4e4ea
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-01 23:00:32 +0100

    Update rag_pipeline.py
    --adding docs_retrieval_processing and rag_classifier functions

--- llm_evaluation/llm_code/rag/rag_pipeline.py
+++ llm_evaluation/llm_code/rag/rag_pipeline.py
@@ -4,8 +4,8 @@ from langchain_community.docstore.document import Document
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from crosslingual_ER.scripts.model_configs import DATA_CONFIG
 from llm_evaluation.llm_code.llm_utility.llm_utility import llm_selection
-from langchain_community.chains import RetrievalQA
 import os
+from langchain_classic.chains import RetrievalQA
 
 
 def store_vectors_database(train_data, keys: dict):
@@ -40,16 +40,43 @@ def rag_retriever(llm_model_name: str, keys:dict, k: int):
     embeddings = GoogleGenerativeAIEmbeddings(
         model = DATA_CONFIG["EMBEDDING_MODEL"]
         )
-    loaded_store = FAISS.load_local(f"{full_path}", 
+    vectorstore = FAISS.load_local(f"{full_path}", 
                                     embeddings,
                                     allow_dangerous_deserialization=True
                                     )
     
-    retr = loaded_store.as_retriever(search_type="similarity", search_kwargs={"k": k})
-    llm = llm_selection(llm_model_name, keys)
-    rag = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=retr,
-        return_source_documents=False
-    )
-    return rag
+    retriever = vectorstore.as_retriever(search_kwargs={"k": k})
+    # llm = llm_selection(llm_model_name, keys)
+    
+    # rag = RetrievalQA.from_chain_type(  
+    #     llm=llm,
+    #     retriever=retr,
+    #     return_source_documents=False
+    # )
+    return retriever
+
+def docs_retrieval_processing(docs):
+  doc_list = []
+  for doc in docs:
+    str_emotion = doc.metadata['emotion'].strip("[]'").replace("'", "")
+    emotion = 'no emotion' if str_emotion == '' else str_emotion
+    text = f" This text is contain emotion: {emotion}"
+    complete_text = doc.page_content + text
+    doc_list.append(complete_text)
+  context = "\n\n".join([x for x in doc_list])
+  return context
+
+def rag_classifier(query, retriever, chain, labels):
+  docs = retriever.invoke(query)
+  context = docs_retrieval_processing(docs)
+  labels.append('no emotion') #adding 'no emotion' label
+  labels_text = ", ".join(labels)
+
+  input_dict = {
+        "context": context,
+        "query": query,
+        "labels": labels_text
+        }
+  result = chain.invoke(input_dict)
+        
+  return result
