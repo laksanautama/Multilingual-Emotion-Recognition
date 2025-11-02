0794d01880adda7127ad847d78db28fdff69e812
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-02 11:55:24 +0100

    Update evaluate.py
    --adding evaluate_rag

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,17 @@
 import argparse
 from utils import load_json_file
 from crosslingual_ER.scripts.model_configs import DATA_CONFIG
-from sklearn.metrics import f1_score
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import (
+    f1_score, accuracy_score, hamming_loss, jaccard_score
+)
+
 
 def evaluate_fs():
     """Function to evaluate few-shot adaptation results."""
     print("Evaluating Few-Shot Adaptation Results...")
 
-    emotion_results = load_json_file("few_shot_results")
+    emotion_results = load_json_file("few_shot_results", 'few_shot')
     sumscore = 0
     for emotion, results in emotion_results.items():
         f1 = f1_score(results["ground_truths"], results["predictions"], average='macro')
@@ -16,8 +20,24 @@ def evaluate_fs():
     print(f"Average F1 Score across all emotions: {sumscore / DATA_CONFIG['NUM_LABELS']}")
 
 
-    pass
+def evaluate_rag():
+    """Function to evaluate RAG adaptation results."""
+    print("Evaluating RAG Adaptation Results...")
 
+    column_names = DATA_CONFIG["LABELS"].append("no emotion")
+    mlb = MultiLabelBinarizer(classes=column_names)
+    mlb.fit([[]])
+    rag_results = load_json_file("rag_evaluation_results", 'rag')
+    y_true = mlb.fit_transform(rag_results["ground_truths"])
+    y_pred = mlb.fit_transform(rag_results["predictions"])    
+    print("Exact Match Ratio:", accuracy_score(y_true, y_pred))
+    print("Hamming Loss:", hamming_loss(y_true, y_pred))
+    print("Micro-F1:", f1_score(y_true, y_pred, average='micro'))
+    print("Macro-F1:", f1_score(y_true, y_pred, average='macro'))
+    print("Jaccard (samples):", jaccard_score(y_true, y_pred, average='samples'))
+
+
+        
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str, required=True, choices=['evaluate_fs', 'evaluate_lora', 'evaluate_rag'], help='Task to perform')
@@ -25,6 +45,8 @@ def main():
 
     if args.task == 'evaluate_fs':
         evaluate_fs()
+    elif args.task == 'evaluate_rag':
+        evaluate_rag()
 
 if __name__ == "__main__":
     main()
