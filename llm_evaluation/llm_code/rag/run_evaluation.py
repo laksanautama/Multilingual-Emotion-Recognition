2227895f11c50f67e458b982917e45fbb9305943
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-02 11:56:37 +0100

    Update run_evaluation.py
    --finishing rag evaluation

--- llm_evaluation/llm_code/rag/run_evaluation.py
+++ llm_evaluation/llm_code/rag/run_evaluation.py
@@ -1,9 +1,9 @@
-from utils import check_faiss_exists, llm_dataset_preparation
-from llm_utility.llm_utility import llm_selection
+from utils import check_faiss_exists, llm_dataset_preparation, create_val_examples, save_results_to_file
+from llm_evaluation.llm_code.llm_utility.llm_utility import llm_selection
 from crosslingual_ER.scripts.model_configs import DATA_CONFIG
+from llm_evaluation.llm_code.llm_utility.llm_config import DIRECTORY_PATH, PROMPT_CONFIG
 from .rag_pipeline import store_vectors_database, rag_retriever, rag_classifier
 from langchain_core.prompts import PromptTemplate
-from llm_utility.llm_utility import classify_text
 import json
 import os
 
@@ -14,24 +14,25 @@ def run_rag(keys: dict, llm_model_name: str):
     print("-" * 20)
 
     MAIN_DIR = (os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-    results_dir = os.path.join(MAIN_DIR, DATA_CONFIG["RAG_RESULTS_DIR"])
+    results_dir = os.path.join(MAIN_DIR, DIRECTORY_PATH["RAG_RESULTS_DIR"])
 
     train_data, val_data = llm_dataset_preparation(DATA_CONFIG["TARGET_TEST_FILENAME"], DATA_CONFIG["TRAIN_SPLIT_SIZE"])
-    
+    val_samples = create_val_examples(val_data, DATA_CONFIG["NUM_VAL_SAMPLES"])
+
     if check_faiss_exists('index') is False:
         store_vectors_database(train_data, keys)
     
     llm = llm_selection(llm_model_name, keys)
     retriever = rag_retriever(llm_model_name, keys, DATA_CONFIG["RAG_TOP_K"])
     classification_prompt = PromptTemplate(
-        template=DATA_CONFIG["RAG_CLASSIFICATION_TEMPLATE"],
+        template=PROMPT_CONFIG["CLF_TEMPLATE"],
         input_variables=["context", "query", "labels"]
     )
     chain = classification_prompt | llm
 
     results = []
     ground_truths = []
-    for _, row in val_data.iterrows():
+    for _, row in val_samples.iterrows():
         print(row['sentence'])
         print("-" * 15)
         label = rag_classifier(
@@ -54,8 +55,10 @@ def run_rag(keys: dict, llm_model_name: str):
         "predictions": results,
         "ground_truths": ground_truths
     }
-    results_filepath = f"{results_dir}/rag_evaluation_results.pkl"
-    with open(results_filepath, "w") as f:
-        json.dump(data_to_save, f, indent=4)
-    print(f"Results saved to {results_filepath}.")
+    results_filename = "rag_evaluation_results"
+    save_results_to_file(data_to_save, results_filename, 'rag')
+    # results_filepath = f"{results_dir}/rag_evaluation_results.json"
+    # with open(results_filepath, "w") as f:
+    #     json.dump(data_to_save, f, indent=4)
+    # print(f"Results saved to {results_filepath}.")
     # Continue from here to implement the evaluation using rag_model on val_data
