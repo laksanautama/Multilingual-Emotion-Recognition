0e1e7cae1ef23dc1706cf641a8c2d7cfb7762805
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-03 17:22:28 +0100

    Update llm_utility.py

--- llm_evaluation/llm_code/llm_utility/llm_utility.py
+++ llm_evaluation/llm_code/llm_utility/llm_utility.py
@@ -3,19 +3,133 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
+from .llm_config import PROMPT_CONFIG, TRANSLATION_MAP
+from crosslingual_ER.scripts.model_configs import DATA_CONFIG
 
-def classify_text(text, chain):
+
+def select_language_config(prompt_language: str):
+    """Select the appropriate prompt configuration based on the specified language."""
+
+    if prompt_language.lower() == 'indonesian' or prompt_language.lower() == 'ind':
+        
+        config = {
+                    'example_template' : PROMPT_CONFIG["IND_EXAMPLE_TEMPLATE"],
+                    'prefix' : PROMPT_CONFIG["IND_PREFIX"],
+                    'suffix' : PROMPT_CONFIG["IND_SUFFIX"],
+                    'clf_template': PROMPT_CONFIG["IND_CLF_TEMPLATE"],
+                    'label_column' : DATA_CONFIG["IND_LABELS"],
+                    'input' : "teks_masukan",
+                    'answer' : "jawaban"
+
+                    }
+        
+    elif prompt_language.lower() == 'balinese' or prompt_language.lower() == 'bal':
+
+        config = {
+                    'example_template' : PROMPT_CONFIG["BAL_EXAMPLE_TEMPLATE"],
+                    'prefix' : PROMPT_CONFIG["BAL_PREFIX"],
+                    'suffix' : PROMPT_CONFIG["BAL_SUFFIX"],
+                    'clf_template' : PROMPT_CONFIG["BAL_CLF_TEMPLATE"],
+                    'label_column' : DATA_CONFIG["BAL_LABELS"],
+                    'input' : "masukan",
+                    'answer' : "pasaut"
+
+                    }
+
+    else:  # Default to English
+        
+        config = {
+                    'example_template' : PROMPT_CONFIG["EN_EXAMPLE_TEMPLATE"],
+                    'prefix' : PROMPT_CONFIG["EN_PREFIX"],
+                    'suffix' : PROMPT_CONFIG["EN_SUFFIX"],
+                    'clf_template' : PROMPT_CONFIG["CLF_TEMPLATE"],
+                    'label_column' : DATA_CONFIG["LABELS"],
+                    'input' : "input",
+                    'answer' : "answer",
+
+                    }
+
+    return config
+
+
+def convert_output_text(output_text: str, prompt_language: str):
+    """Convert the output text from the LLM to standardized 'yes' or 'no'."""
+    output_text = output_text.strip().lower()
+    if prompt_language.lower() == 'indonesian' or prompt_language.lower() == 'ind':
+        if '\n\njawaban: ya' in output_text:
+            return 'yes'
+        elif '\n\njawaban: tidak' in output_text:
+            return 'no'
+        else:
+            # Return a default or handle cases where neither 'ya' nor 'tidak' is found
+            return 'no'
+    elif prompt_language.lower() == 'balinese' or prompt_language.lower() == 'bal':
+        if '\n\npasaut: inggih' in output_text:
+            return 'yes'
+        elif '\n\npasaut: ten' in output_text:
+            return 'no'
+        else:
+            # Return a default or handle cases where neither 'inggih' nor 'ten' is found
+            return 'no'
+        
+    else:  # Default to English
+        if '\n\nanswer: yes' in output_text:
+            return 'yes'
+        elif '\n\nanswer: no' in output_text:
+            return 'no'
+        else:
+            # Return a default or handle cases where neither 'yes' nor 'no' is found
+            return 'no'
+
+def classify_text(text, chain, prompt_language: str):
     """Classify the input text using the provided LLM chain."""
     result = chain.invoke({"input": text})
     output_text = result.content.strip().lower()
-    if '\n\nanswer: yes' in output_text:
-        return 'yes'
-    elif '\n\nanswer: no' in output_text:
-        return 'no'
-    else:
-    # Return a default or handle cases where neither 'yes' nor 'no' is found
-        return 'no'
+
+    print(f"=== {output_text} ===")
+
+    return convert_output_text(output_text, prompt_language), output_text
+
+def translate_label(label: str, prompt_language: str):
+    word = label.lower()
+    lang = prompt_language.lower()
+
+    if lang == 'english':
+        return word
     
+    if word in TRANSLATION_MAP:
+        if lang in TRANSLATION_MAP[word]:
+            return TRANSLATION_MAP[word][lang]
+        else:
+            return f"Error: Translation for '{word}' to '{lang}' not found."
+    else:
+        return f"Error: English word '{word}' not found in map."
+
+def translate_emotion_text(prompt_language: str):
+    lang = prompt_language.lower()
+    if lang == 'indonesian':
+        return " Teks ini mengandung emosi: "
+    elif lang == 'balinese':
+        return " Tulisan puniki madaging emosi: "
+    else:
+        return " This text is contain emotion: "
+
+def translate_answer(answer: str, prompt_language: str):
+    ans = answer.lower()
+    lang = prompt_language.lower()
+
+    if lang == 'english':
+        return ans
+    elif lang == 'indonesian':
+        if ans == 'yes':
+            return 'ya'
+        elif ans == 'no':
+            return 'tidak'
+    else:
+        if ans == 'yes':
+            return 'inggih'
+        elif ans == 'no':
+            return 'ten'
 
 def llm_selection(llm_model_name: str, keys: dict):
     """Select and return the appropriate LLM model based on the provided name."""
