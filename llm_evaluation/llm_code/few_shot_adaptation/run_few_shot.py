d7aa638509abc9a0c5aab2e97d637895f5c13289
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-03 17:22:05 +0100

    Update run_few_shot.py

--- llm_evaluation/llm_code/few_shot_adaptation/run_few_shot.py
+++ llm_evaluation/llm_code/few_shot_adaptation/run_few_shot.py
@@ -1,6 +1,6 @@
 # from llm_model_selection import llm_selection
-from llm_evaluation.llm_code.llm_utility.llm_utility import llm_selection, classify_text
-from utils import llm_dataset_preparation, create_train_examples, create_val_examples, save_results_to_file
+from llm_evaluation.llm_code.llm_utility.llm_utility import llm_selection, classify_text, select_language_config
+from utils import llm_dataset_preparation, create_train_examples, create_val_examples, save_results_to_file, save_analysis_results
 from crosslingual_ER.scripts.model_configs import DATA_CONFIG
 from ..llm_utility.llm_config import PROMPT_CONFIG
 import logging
@@ -9,7 +9,7 @@ from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
 
 
 
-def run_fs(keys: dict, llm_model_name: str):
+def run_fs(keys: dict, llm_model_name: str, prompt_language: str):
     
     # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -23,41 +23,53 @@ def run_fs(keys: dict, llm_model_name: str):
     val_data_samples = create_val_examples(val_data, DATA_CONFIG["NUM_VAL_SAMPLES"])
 
     emotion_results = {}
-
+    emotion_analysis = {}
+    
+    config = select_language_config(prompt_language)
     
     for emotion in DATA_CONFIG["LABELS"]:
         print(f"\nAdapting and Evaluating for Emotion: {emotion}")
-        train_examples = create_train_examples(train_data, emotion, DATA_CONFIG["NUM_TRAIN_SAMPLES_PER_CLASS"])
+        train_examples = create_train_examples(train_data, emotion, DATA_CONFIG["NUM_TRAIN_SAMPLES_PER_CLASS"], prompt_language)
         fs_prompts = FewShotPromptTemplate(
                     examples=train_examples,
                     example_prompt = PromptTemplate(
-                                    input_variables=["input", "answer"],
-                                    template=PROMPT_CONFIG["EXAMPLE_TEMPLATE"]
+                                    input_variables=[config['input'], config['answer']],
+                                    template=config['example_template']
                     ),
-                    prefix=PROMPT_CONFIG["EN_PREFIX"].format(emotion=emotion), 
-                    suffix=PROMPT_CONFIG["EN_SUFFIX"],
+                    prefix=config['prefix'].format(emotion=emotion), 
+                    suffix=config['suffix'],
                     input_variables=["input"],
                     example_separator="\n"
             )
         chain = fs_prompts | llm_model
         predictions = []
         ground_truths = []
+        text = []
+        justification = []
 
         for i in range(len(val_data_samples)):
-            result = classify_text(val_data_samples['sentence'].iloc[i], chain)
+            result, output_text = classify_text(val_data_samples['sentence'].iloc[i], chain, prompt_language)
             ground_truth = 'yes' if val_data_samples[emotion].iloc[i] == 1 else 'no'
-            #print input, prediction, ground_truth but i'll keep it silent for now
             print(f"Input: {val_data_samples['sentence'].iloc[i]}")
             print(f"Prediction: {result}")
             print(f"Ground Truth: {ground_truth}")
             print("+"*20)
             predictions.append(result)
             ground_truths.append(ground_truth)
+            text.append(val_data_samples[emotion].iloc[i])
+            justification.append(output_text)
+        
+
         
         emotion_results[emotion] = {"predictions": predictions, 
                                     "ground_truths": ground_truths
                                     }
+        
+    emotion_analysis[prompt_language] = {'text': text,
+                                         'justification': justification}
 
     results_filename = "few_shot_results"
-    save_results_to_file(emotion_results, results_filename, 'few_shot')
+    mer_analysis_filename = "few_shot_analysis"
+    save_results_to_file(emotion_results, results_filename, 'few_shot', prompt_language)
+    save_analysis_results(emotion_analysis, mer_analysis_filename, 'few_shot', prompt_language)
     
