43fb33998cb2fa052674613f58206e88b556653c
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-11 15:50:44 +0100

    Update evaluate_lora.py
    revised predict function

--- llm_evaluation/llm_code/Lora/evaluate_lora.py
+++ llm_evaluation/llm_code/Lora/evaluate_lora.py
@@ -1,4 +1,7 @@
 from crosslingual_ER.scripts.model_configs import MODEL_PATHS
+from .lora_config import load_lora_config
+import torch
+from utils import save_lora_results
 from llm_evaluation.llm_code.llm_utility.llm_config import PROMPT_CONFIG, DIRECTORY_PATH
 
 import os
@@ -18,10 +21,14 @@ from peft import (
     PeftModel
 )
 
-def inference_lora(val_dataset, bnb_config):
-
+def inference_lora(val_dataset):
+    print("-" * 20)
+    print("Starting LoRA Inference...")
+    print("-" * 20)
     PARENT_DIR = (os.path.dirname(os.path.dirname(__file__)))
     MODEL_DIR = os.path.join(PARENT_DIR, DIRECTORY_PATH["LORA_MODEL_DIR"])
+
+    bnb_config, _ = load_lora_config()
 
     base = AutoModelForCausalLM.from_pretrained(
             MODEL_PATHS["meta_llama"],
@@ -29,32 +36,50 @@ def inference_lora(val_dataset, bnb_config):
             device_map="auto",
             )
     
-    def predict(text):
-        prompt = (
-                    f"{SYSTEM_INSTRUCT}\n"
-                    f"Text: {text}\n"
-                    f"Answer:"
-                )
+    def predict(text, tokenizer):
 
-        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+        prompt = (
+                f"{SYSTEM_INSTRUCT}\n"
+                f"Text: {text}\n"
+                f"Answer:"
+            )
+        
+        tokenizer.pad_token = tokenizer.eos_token
+
+        inputs = tokenizer(prompt, return_tensors="pt", padding=True).to(model.device)
+        input_ids = inputs["input_ids"]
+
         with torch.no_grad():
-            output = model.generate(
-                                    **inputs,
-                                    max_new_tokens=30,
-                                    temperature=0.3
-                        )
-            
-        decoded = tokenizer.decode(output[0], skip_special_tokens=True)
-        return decoded.split("Answer:")[-1].strip()
+            output_ids = model.generate(
+                    input_ids,
+                    max_new_tokens=10,
+                    eos_token_id=tokenizer.eos_token_id,
+                    do_sample=False,
+                    temperature=0.3
+                    )
+        new_tokens = output_ids[0, input_ids.shape[1]:]
+        decoded = tokenizer.decode(new_tokens, skip_special_tokens=True).strip()
+        emotions_str = decoded.split("###")[0].strip()
+        emotions_list = [e.strip().replace('.', '') for e in emotions_str.split(',') if e.strip()]
+        return emotions_list
     
     model = PeftModel.from_pretrained(base, MODEL_DIR)
     tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
 
+    predictions = []
     for _, row in val_dataset.iterrows():
-        prediction = predict(row['sentence'])
+        prediction = predict(row['sentence'], tokenizer)
+        predictions.append({
+            'sentence': row['sentence'],
+            'true_emotions': row['emotions'],
+            'predicted_emotions': prediction
+        })
+
         print(f"Input Text: {row['sentence']}")
         print(f"Predicted Emotions: {prediction}")
         print("-" * 20)
+    
+    save_lora_results(predictions)
 
 
 
