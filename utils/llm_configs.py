f702b9910621c5c81a3166d30d94a351587e2e39
laksanautama <155938070+laksanautama@users.noreply.github.com>
2025-11-17 15:09:17 +0100

    Create llm_configs.py

---
+++ utils/llm_configs.py
@@ -0,0 +1,142 @@
+# LLM PROMPT CONFIGURATIONS
+#------------------------------
+
+PROMPT_CONFIG = {
+    "EN_EXAMPLE_TEMPLATE" : """
+                            Input: {input}
+                            Answer: {answer}
+                            """,
+
+    "IND_EXAMPLE_TEMPLATE" : """
+                            Teks Masukan: {teks_masukan}
+                            Jawaban: {jawaban}
+                            """,
+
+    "BAL_EXAMPLE_TEMPLATE" : """
+                            Masukan: {masukan}
+                            Pasaut: {pasaut}
+                            """,
+
+    "EN_PREFIX" : """Analyze the text below for the presence of {emotion}. Explain your reasoning briefly and conclude with 'Answer:' followed by either 'yes' or 'no'.
+
+                    Here are some examples:
+                    """,
+
+    "EN_SUFFIX" : """
+                    Input: {input}
+                    Answer:""",
+
+    "IND_PREFIX" : """Analisis teks berikut apakah termasuk ungkapan {emotion}. Jelaskan alasan Anda secara singkat dan disertai dengan 'Jawaban:' 'ya' atau 'tidak'.
+                    Berikut ini adalah beberapa contoh:
+                    """,
+
+    "IND_SUFFIX" : """
+                    Teks Masukan: {input}
+                    Jawaban:""",
+
+    "BAL_PREFIX" : """Analisisang teks ring ngidangang puniki indik wentenang {emotion}. Terangang alesan ragane sekadi ring sekancan ring pungkuran tur puputang antuk 'Pasaut:' kalanturang olih 'inggih' utawi 'ten'.
+                    Puniki makudang-kudang conto:
+                    """,
+
+    "BAL_SUFFIX" : """
+                    Masukan: {input}
+                    Pasaut:""",
+                    
+    "CLF_TEMPLATE"  : """
+                    You are a emotion classification assistant.
+                    Your task is to assign labels to the query based ONLY on the context provided.
+
+                    Context:
+                    {context}
+
+                    Query:
+                    {query}
+
+                    Labels:
+                    {labels}
+
+                    Instruction:
+                    Choose most appropriate labels from the list above.
+                    You can choose more than one label. Conclude your decision with 'Answer: ' followed which labels from the list above that suited for this query, and put the reason of your choice after 'Reason: '.
+                    """,
+
+    "IND_CLF_TEMPLATE"  : """
+                    Anda adalah asisten AI yang bertugas untuk mengklasifikasi emosi yang terkandung pada teks.
+                    Tugas Anda adalah memberikan label pada teks berdasarkan KONTEKS yang diberikan.
+
+                    Konteks:
+                    {context}
+
+                    Teks:
+                    {query}
+
+                    Label:
+                    {labels}
+
+                    Instruksi:
+                    Pilih label yang paling sesuai dari daftar di atas.
+                    Anda dapat memilih lebih dari satu label. Putuskanlah jawaban anda dengan kata 'Jawaban: ' diikuti dengan label dari daftar di atas yang sesuai untuk pertanyaan ini, dan tuliskan penjelaskan atas pilihan anda tersebut setelah kata 'Alasan: '
+                    """,
+
+    "BAL_CLF_TEMPLATE" : """
+                    Ragane wantah asisten AI sané madué tugas ngeklasifikasiang emosi sané wénten ring teks. Tugas ragane inggih punika ngicen label teks manut KONTEKS sane kaicen.
+                    
+                    Konteks:
+                    {context}
+
+                    teks:
+                    {query}
+
+                    Label:
+                    {labels}
+
+                    Patunjuk:
+                    Pilih label sané pinih patut saking daftar ring ajeng. Ragane dados milih langkungan saking asiki label. Tentuang pasaut ragane antuk kruna 'Pasaut:' kalanturang antuk label saking daftar ring ajeng sane manggeh ring pitaken puniki, tur tulis penjelasan indik pilihan semetone sesampun kruna 'Alasan:'
+                    """,
+
+    "SYSTEM_INSTRUCT" :"""### Emotion Recognition Task
+                        Given the text below, identify all emotions that apply.
+                        Available emotions: anger, disgust, fear, joy, sadness, surprise, no emotion.
+                        """
+
+}
+
+DIRECTORY_PATH = {
+    "FEW_SHOT_RESULTS_DIR": "results/fs",
+    "RAG_RESULTS_DIR": "results/rag",
+    "LORA_RESULTS_DIR": "results/lora",
+    "LORA_CHECKPOINTS_DIR": "llm_evaluation/lora_checkpoints",
+    "LORA_MODEL_DIR": "llm_evaluation/lora_model_save"
+}
+
+TRANSLATION_MAP = {
+
+    'anger': {
+        'indonesian': 'marah',
+        'balinese': 'gedeg'
+    },
+    'disgust': {
+        'indonesian': 'jijik',
+        'balinese': 'seneb'
+    },
+    'fear': {
+        'indonesian': 'takut',
+        'balinese': 'takut'
+    },
+    'joy': {
+        'indonesian': 'senang',
+        'balinese': 'sukha'
+    },
+    'sadness': {
+        'indonesian': 'sedih',
+        'balinese': 'sedih'
+    },
+    'surprise': {
+        'indonesian': 'terkejut',
+        'balinese': 'makesiab'
+    },
+    'no emotion': {
+        'indonesian': 'tanpa emosi',
+        'balinese' : 'ten meduwe emosi'
+    }
+}
